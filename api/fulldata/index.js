e394bdc13978a8cd8c8a973375ce84a0ba7a1a88
CeciliaBot <giovalin.psn@gmail.com>
2023-02-28 20:09:40 +0100

    test12211212

--- api/fulldata/index.js
+++ api/fulldata/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     await cors(req, res)
     try {
         res.setHeader('Content-Type', 'application/json');
-        res.json({
+        return res.json({
             heroes: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'heroes.json'))),
             artifacts: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'artifacts.json'))),
             buffs: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'buffs.json'))),
