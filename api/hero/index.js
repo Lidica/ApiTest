9a0169df5d621f6ce3294b58682bfdb42b215c98
CeciliaBot <giovalin.psn@gmail.com>
2023-02-28 18:55:05 +0100

    fix3

--- api/hero/index.js
+++ api/hero/index.js
@@ -1,8 +1,8 @@
 import HeroList from '../../js/hero-list.js'
 import { cors } from '../../js/middleware.js'
 
-export default function handler(req, res) {
-    cors(req, res);
+export default async function handler(req, res) {
+    await cors(req, res);
     res.setHeader('Content-Type', 'application/json');
     return res.end(JSON.stringify(HeroList));
 }
