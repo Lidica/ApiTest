052c242a87e68cdb39b489c924408c9603ea00fa
CeciliaBot <giovalin.psn@gmail.com>
2023-02-28 18:45:07 +0100

    cors

--- api/hero/[id].js
+++ api/hero/[id].js
@@ -1,16 +1,15 @@
 import HeroDB from '../../js/heroes.js'
+import { cors } from '../../js/middleware.js'
 
 export default function handler(req, res) {
-    try {
-        const { id } = req.query;
-        var hero = HeroDB[id];
-        if (hero) {
-            res.setHeader('Content-Type', 'application/json');
-            return res.end(JSON.stringify(hero))
-        } else {
-            return res.end('{"status": 404, "message": "Not found"}')
-        }
-    } catch(err) {
-        return res.end(err.message)
+    cors(req, res)
+    
+    const { id } = req.query;
+    var hero = HeroDB[id];
+    if (hero) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(hero))
+    } else {
+        return res.end('{"status": 404, "message": "Not found"}')
     }
 }
--- api/hero/index.js
+++ api/hero/index.js
@@ -1,6 +1,8 @@
 import HeroList from '../../js/hero-list.js'
+import { cors } from '../../js/middleware.js'
 
 export default function handler(req, res) {
+    cors(req, res);
     res.setHeader('Content-Type', 'application/json');
     return res.end(JSON.stringify(HeroList));
 }
