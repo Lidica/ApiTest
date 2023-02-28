9251792fa80598f8b47b6208b4e52d420085ee4d
CeciliaBot <giovalin.psn@gmail.com>
2023-02-28 19:03:07 +0100

    aaaaaaaa

--- api/fulldata/index.js
+++ api/fulldata/index.js
@@ -1,13 +1,16 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
 export default async function handler(req, res) {
     try {
         res.setHeader('Content-Type', 'application/json');
         res.json({
-            heroes: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'heroes.json'))),
-            artifacts: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'artifacts.json'))),
-            buffs: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'buffs.json'))),
-            debuffs: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'debuffs.json'))),
-            common: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'common.json'))),
-            enhancements: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'db', 'enhancements.json')))
+            heroes: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'heroes.json'))),
+            artifacts: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'artifacts.json'))),
+            buffs: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'buffs.json'))),
+            debuffs: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'debuffs.json'))),
+            common: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'common.json'))),
+            enhancements: JSON.parse(readFileSync(path.join(process.cwd(), 'db', 'enhancements.json')))
         })
     } catch (err) {
         return res.status(200).end("\"code\": 400, \"message\": "+err.message)
